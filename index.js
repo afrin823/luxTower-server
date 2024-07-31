ac415594e1bb7b741d380a7a284538be50914cb0
afrin823 <afrin12490@gmail.com>
2024-07-31 20:43:02 +0600

    get aprtment

--- index.js
+++ index.js
@@ -26,12 +26,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const apartmentCollection = client.db("AppermentDB").collection("appermentCollection");
+    const coponCollection = client.db("cuponDB").collection("couponCollection");
+
+    //apartment
+    app.get('/apartment', async(req, res) => {
+      const result = await apartmentCollection.find().toArray();
+      res.send(result);
+    })
+    //coupon
+    app.get('/coupon', async(req, res) => {
+      const result = await coponCollection.find().toArray();
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
