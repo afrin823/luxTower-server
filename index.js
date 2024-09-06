b5735353d03c6c94862ca622525059c59af8f3ca
afrin823 <afrin12490@gmail.com>
2024-09-06 23:15:21 +0600

    ssl commreze payment system

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz-lts')
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -29,9 +30,13 @@ const client = new MongoClient(uri, {
   }
 });
 
+const store_id = process.env.STORE_ID
+const store_passwd = process.env.SOTRE_PASS
+const is_live = false //true for live, false for sandbox
+
 async function run() {
   try {
-    await client.connect(); 
+    await client.connect();
 
     const apartmentCollection = client.db("AppermentDB").collection("appermentCollection");
     const couponCollection = client.db("cuponDB").collection("couponCollection");
@@ -42,246 +47,277 @@ async function run() {
 
     // Payment intent
     app.post('/create-payment-intent', async (req, res) => {
-      const { price } = req.body;
-      const amount = parseInt(price * 100);
+      const paymentInfo = req.body;
+      const tran_id = new ObjectId().toString();
 
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount,
-        currency: "usd",
-        payment_method_types: ['card']
+      const data = {
+        total_amount: paymentInfo.apartmentRent,
+        currency: 'BDT',
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: 'http://localhost:3030/success',
+        fail_url: 'http://localhost:3030/fail',
+        cancel_url: 'http://localhost:3030/cancel',
+        ipn_url: 'http://localhost:3030/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: paymentInfo.userInfo.name,
+        cus_email: paymentInfo.userInfo.email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: paymentInfo.userInfo.name,
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+      };
+      console.log(data);
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+      sslcz.init(data).then(apiResponse => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL
+        res.redirect(GatewayPageURL)
+        console.log('Redirecting to: ', GatewayPageURL)
       });
-      res.send({
-        clientSecret: paymentIntent.client_secret
-      });
-    });
+    })
 
-    // JWT token generation
-    app.post('/jwt', async (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-      res.send({ token });
-    });
 
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.params.email;
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
-      if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' });
+  // JWT token generation
+  app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+    res.send({ token });
+  });
+
+  const verifyAdmin = async (req, res, next) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if (!isAdmin) {
+      return res.status(403).send({ message: 'forbidden access' });
+    }
+    next();
+  };
+  // Users related API
+  app.get('/users', async (req, res) => {
+    const result = await userCollection.find().toArray();
+    res.send(result);
+  });
+
+  app.get('/users/admin/:email', async (req, res) => {
+    const email = req.params.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if (user) {
+      admin = user?.role === 'admin';
+    }
+    res.send({ admin });
+  });
+
+  app.post("/users", async (req, res) => {
+    const data = req.body;
+
+    // Check if the email already exists
+    const existingUser = await userCollection.findOne({ email: data.email });
+
+    if (existingUser) {
+      res.send({ message: "Login Success" });
+    } else {
+      const doc = {
+        name: data.name,
+        email: data.email,
+        role: "",
+      };
+      await userCollection.insertOne(doc);
+      res.send({ message: "Registration Success" });
+    }
+  });
+
+  app.patch('/users/admin/:id', verifyAdmin, async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updateDoc = {
+      $set: {
+        role: "admin"
       }
-      next();
     };
-    // Users related API
-    app.get('/users',  async (req, res) => {
-      const result = await userCollection.find().toArray();
+    const result = await userCollection.updateOne(filter, updateDoc);
+    res.send(result);
+  });
+
+
+  // Delete users
+  app.delete('/users/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await userCollection.deleteOne(query);
+    res.send(result);
+  });
+
+  //members
+  app.get("/members", async (req, res) => {
+    const query = { role: "member" };
+    const result = (await userCollection.find(query).toArray()).reverse();
+    res.send(result);
+  });
+
+  app.put("/members/:id", async (req, res) => {
+    const id = req.params;
+    const filter = { _id: new ObjectId(id) };
+    const doc = {
+      $set: {
+        role: "",
+      },
+    };
+    await userCollection.updateOne(filter, doc);
+    res.send({ message: "Member successfully removed." });
+  });
+
+  // Announcements
+  app.get("/announcements", async (req, res) => {
+    const result = (await announcements.find().toArray()).reverse();
+    res.send(result);
+  });
+
+  app.post("/announcements", async (req, res) => {
+    const body = req.body;
+    const result = await announcements.insertOne(body);
+    res.send(result);
+  });
+
+  app.get("/usersRole", async (req, res) => {
+    const query = req.query;
+
+    if (!query.email) {
+      return res.status(400).send({ message: "Unauthorized" });
+    }
+
+    const result = await userCollection.findOne({ email: query.email });
+    res.send(result);
+  });
+
+  // Apartments endpoint
+  app.get('/apartment', async (req, res) => {
+    try {
+      const result = await apartmentCollection.find().toArray();
       res.send(result);
-    });
+    } catch (error) {
+      res.status(500).send({ error: "Failed to fetch apartments." });
+    }
+  });
 
-    app.get('/users/admin/:email', async (req, res) => {
+  // Coupons endpoint
+  app.get('/coupon', async (req, res) => {
+    try {
+      const result = (await couponCollection.find().toArray()).reverse();
+      res.send(result);
+    } catch (error) {
+      res.status(500).send({ error: "Failed to fetch coupons." });
+    }
+  });
+
+  // Booked apartments endpoints
+  app.get("/bookedApartments/:email", async (req, res) => {
+    try {
       const email = req.params.email;
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user?.role === 'admin';
-      }
-      res.send({ admin });
-    });
+      const filter = { email: email };
+      const result = await wishlistCollection.find(filter).toArray();
+      res.send(result);
+    } catch (error) {
+      res.status(500).send("Error fetching user's apartments");
+    }
+  });
 
-     app.post("/users", async (req, res) => {
-      const data = req.body;
-
-      // Check if the email already exists
-      const existingUser = await userCollection.findOne({ email: data.email });
-
-      if (existingUser) {
-        res.send({ message: "Login Success" });
-      } else {
-        const doc = {
-          name: data.name,
-          email: data.email,
-          role: "",
-        };
-        await userCollection.insertOne(doc);
-        res.send({ message: "Registration Success" });
-      }
-    });
-
-     app.patch('/users/admin/:id', verifyAdmin, async (req, res) => {
+  app.patch("/bookedApartments/:id", async (req, res) => {
+    try {
       const id = req.params.id;
+      const { status } = req.body;
       const filter = { _id: new ObjectId(id) };
+      const apartment = await wishlistCollection.findOne(filter);
+
+      if (!apartment) {
+        return res.status(404).send({ status: 404, message: "Apartment not found" });
+      }
+
+      const userEmail = apartment.userInfo.email;
+      const filter2 = { email: userEmail };
+
+      const today = new Date();
+      const day = String(today.getDate()).padStart(2, "0");
+      const month = String(today.getMonth() + 1).padStart(2, "0");
+      const year = today.getFullYear();
+      const formattedDate = `${day}/${month}/${year}`;
+
       const updateDoc = {
         $set: {
-          role: "admin"
-        }
-      };
-      const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result);
-    });
-    
-
-    // Delete users
-    app.delete('/users/:id', async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    //members
-    app.get("/members", async (req, res) => {
-      const query = { role: "member" };
-      const result = (await userCollection.find(query).toArray()).reverse();
-      res.send(result);
-    });
-
-    app.put("/members/:id", async (req, res) => {
-      const id = req.params;
-      const filter = { _id: new ObjectId(id) };
-      const doc = {
-        $set: {
-          role: "",
+          status: status || "checked",
+          accept_date: formattedDate,
         },
       };
-      await userCollection.updateOne(filter, doc);
-      res.send({ message: "Member successfully removed." });
-    });
 
-    // Announcements
-    app.get("/announcements", async (req, res) => {
-      const result = (await announcements.find().toArray()).reverse();
-      res.send(result);
-    });
+      await wishlistCollection.updateOne(filter, updateDoc);
+      res.send({ status: 200, message: "Agreement Acceptance Successful" });
+    } catch (error) {
+      res.status(500).send({ status: 500, message: "Something went wrong! Please try later." });
+    }
+  });
 
-    app.post("/announcements", async (req, res) => {
-      const body = req.body;
-      const result = await announcements.insertOne(body);
-      res.send(result);
-    });
+  app.get("/bookedApartments", async (req, res) => {
+    const bookedApartments = req.body;
+    const result = await wishlistCollection.find().toArray();
+    res.send(result);
+  });
 
-    app.get("/usersRole", async (req, res) => {
-      const query = req.query;
+  app.post("/bookedApartments", async (req, res) => {
+    const apartmentInfo = req.body;
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, "0");
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const year = now.getFullYear();
+    const requestDate = `${day}/${month}/${year}`;
 
-      if (!query.email) {
-        return res.status(400).send({ message: "Unauthorized" });
-      }
+    const doc = {
+      apartment_id: apartmentInfo.apartment_id,
+      email: apartmentInfo.email,
+      status: "pending",
+      request_date: requestDate,
+    };
 
-      const result = await userCollection.findOne({ email: query.email });
-      res.send(result);
-    });
+    const userEmail = apartmentInfo.email;
+    const query = { "email": userEmail };
 
-    // Apartments endpoint
-    app.get('/apartment', async (req, res) => {
-      try {
-        const result = await apartmentCollection.find().toArray();
-        res.send(result);
-      } catch (error) {
-        res.status(500).send({ error: "Failed to fetch apartments." });
-      }
-    });
+    const userRole = await userCollection.findOne({ email: userEmail });
 
-    // Coupons endpoint
-    app.get('/coupon', async (req, res) => {
-      try {
-        const result = (await couponCollection.find().toArray()).reverse();
-        res.send(result);
-      } catch (error) {
-        res.status(500).send({ error: "Failed to fetch coupons." });
-      }
-    });
+    if (userRole.role === "admin") {
+      res.send({ message: "It's not available for you. Sorry!" });
+    } else {
+      const isExist = await wishlistCollection.findOne(query);
 
-    // Booked apartments endpoints
-    app.get("/bookedApartments/:email", async (req, res) => {
-      try {
-        const email = req.params.email;
-        const filter = { email: email };
-        const result = await wishlistCollection.find(filter).toArray();
-        res.send(result);
-      } catch (error) {
-        res.status(500).send("Error fetching user's apartments");
-      }
-    });
-
-    app.patch("/bookedApartments/:id", async (req, res) => {
-      try {
-        const id = req.params.id;
-        const { status } = req.body;
-        const filter = { _id: new ObjectId(id) };
-        const apartment = await wishlistCollection.findOne(filter);
-
-        if (!apartment) {
-          return res.status(404).send({ status: 404, message: "Apartment not found" });
-        }
-
-        const userEmail = apartment.userInfo.email;
-        const filter2 = { email: userEmail };
-
-        const today = new Date();
-        const day = String(today.getDate()).padStart(2, "0");
-        const month = String(today.getMonth() + 1).padStart(2, "0");
-        const year = today.getFullYear();
-        const formattedDate = `${day}/${month}/${year}`;
-
-        const updateDoc = {
-          $set: {
-            status: status || "checked",
-            accept_date: formattedDate,
-          },
-        };
-
-        await wishlistCollection.updateOne(filter, updateDoc);
-        res.send({ status: 200, message: "Agreement Acceptance Successful" });
-      } catch (error) {
-        res.status(500).send({ status: 500, message: "Something went wrong! Please try later." });
-      }
-    });
-
-    app.get("/bookedApartments", async (req, res) => {
-      const bookedApartments = req.body;
-      const result = await wishlistCollection.find().toArray();
-      res.send(result);
-    });
-
-    app.post("/bookedApartments", async (req, res) => {
-      const apartmentInfo = req.body;
-      const now = new Date();
-      const day = String(now.getDate()).padStart(2, "0");
-      const month = String(now.getMonth() + 1).padStart(2, "0");
-      const year = now.getFullYear();
-      const requestDate = `${day}/${month}/${year}`;
-
-      const doc = {
-        apartment_id: apartmentInfo.apartment_id,
-        email: apartmentInfo.email,
-        status: "pending",
-        request_date: requestDate,
-      };
-
-      const userEmail = apartmentInfo.email;
-      const query = { "email": userEmail };
-
-      const userRole = await userCollection.findOne({ email: userEmail });
-
-      if (userRole.role === "admin") {
-        res.send({ message: "It's not available for you. Sorry!" });
+      if (isExist) {
+        res.send({ message: "User has already booked an apartment." });
       } else {
-        const isExist = await wishlistCollection.findOne(query);
-
-        if (isExist) {
-          res.send({ message: "User has already booked an apartment." });
-        } else {
-          await wishlistCollection.insertOne(doc);
-          res.send({ status: 200, message: "Apartment booking success" });
-        }
+        await wishlistCollection.insertOne(doc);
+        res.send({ status: 200, message: "Apartment booking success" });
       }
-    });
+    }
+  });
 
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
+  await client.db("admin").command({ ping: 1 });
+  console.log("Pinged your deployment. You successfully connected to MongoDB!");
+} finally {
+  // await client.close();
+}
 }
 
 run().catch(console.dir);
